050f012e4e58133693e83a0f9cd7bbb4bed21df8
andreyverzin <andrew@watechnology.com>
2024-03-27 18:28:30 +0400

    changed layout

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
                 <div className='background-image' />
                 <div className='category-body-container'>
                     <h2>{title}</h2>
-                    <p>Shop Now</p>
+                    <p>Shop Now!</p>
                 </div>
             </div>
         ))}
